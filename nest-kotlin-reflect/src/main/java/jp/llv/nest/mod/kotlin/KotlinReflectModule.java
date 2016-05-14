e112787d43547ecd969990c3f2351fc3f232efcd
toyblocks <a@llv.jp>
2016-05-14 15:50:32 +0900

    Remove version requirement

--- nest-kotlin-reflect/src/main/java/jp/llv/nest/mod/kotlin/KotlinReflectModule.java
+++ nest-kotlin-reflect/src/main/java/jp/llv/nest/mod/kotlin/KotlinReflectModule.java
@@ -6,7 +6,6 @@
 package jp.llv.nest.mod.kotlin;
 
 import jp.llv.nest.module.Module;
-import jp.llv.nest.module.ReqVer;
 
 /**
  *
@@ -14,6 +13,6 @@ import jp.llv.nest.module.ReqVer;
  */
 @Module(name="kotlin-reflect", author = "toyblocks", version=1)
 public class KotlinReflectModule {
-    public KotlinReflectModule(@ReqVer(min = 1, max = 1) KotlinModule module) {
+    public KotlinReflectModule(KotlinModule module) {
     }
 }
